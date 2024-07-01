04f8670bd803b9cff97ca2a49ba3f860aaff3abc
valentinooo <valeartem@gmail.com>
2024-07-01 18:52:10 +0300

    Практична ШАГ.Бойлер

--- Project_Git/Project1/File1.cpp
+++ Project_Git/Project1/File1.cpp
@@ -19,6 +19,7 @@ struct Machine
     int height;
     int power;
     int speed_of_work;
+    bool water_vapour;  //true or false
     Temperature temperature;
 
 
@@ -29,9 +30,19 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     Machine object1;
-    cin >> object1.Brand >> object1.color >> object1.width >> object1.height >> object1.length >> object1.power >> object1.speed_of_work;
+    cin >> object1.Brand >> object1.color >> object1.width >> object1.height >> object1.length >> object1.power >> object1.water_vapour;
+
     cin >> object1.temperature.high >> object1.temperature.low >> object1.temperature.freeze;
-    cout <<"1-Brand\n2-color\n3-width\n4-height\n5-length\n6-power\n7-speed_of_work\n"<< object1.Brand <<endl<< object1.color <<endl<< object1.width <<"meters"<<endl << object1.height << "meters" << endl << object1.length << "meters" << endl << object1.power <<"kW" << endl << object1.speed_of_work<<"rps"<<endl;
-    cout<<"temperature:1-high\n2-low\n3-freeze\n"<< object1.temperature.high <<"*C"<<endl<< object1.temperature.low << "*C" << endl << object1.temperature.freeze << "*C" << endl;
+    cin >> object1.speed_of_work;
+    cout << "1-Brand\n2-color\n3-width\n4-height\n5-length\n6-power\n7-speed_of_work\n8-water_vapour(1-yes,0-no)\n" << object1.Brand << endl << object1.color << endl << object1.width << "meters" << endl << object1.height << "meters" << endl << object1.length << "meters" << endl << object1.power << "kW" << endl << object1.speed_of_work << "rps" << endl;
+    if (object1.water_vapour)
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "0" << endl;
+    }
+    cout << "temperature:1-high\n2-low\n3-freeze\n" << object1.temperature.high << "*C" << endl << object1.temperature.low << "*C" << endl << object1.temperature.freeze << "*C" << endl;
     return 0;
 }
